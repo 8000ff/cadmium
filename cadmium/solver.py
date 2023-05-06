1d6816eb0c1acb2d00b3ebfdd6fcb74b9a8fc7ed
8000ff <->
2023-05-06 18:15:35 +0200

    fix: remove redundant operations from the simple solver

--- cadmium/solver.py
+++ cadmium/solver.py
@@ -9,7 +9,7 @@ import numpy as np
 # use safe height policy, will Z hop if cuts are not contiguous
 # will insert a pause between every operation
 # work in machine coordinates
-def safe_height_solver(job : Job, machine: Machine, safe_height: float = 0,dwell: float = 3, eps : float = .01):
+def safe_height_solver(job : Job, machine: Machine, safe_height: float = 0,dwell: float = 3, eps : float = .01,pause_every_op : bool = False):
     order_of_operations : List[Operation] = list(nx.dfs_preorder_nodes(job.operations))
     order_of_operations = order_of_operations[1:]
     yield GCodeAbsoluteDistanceMode()
@@ -20,7 +20,7 @@ def safe_height_solver(job : Job, machine: Machine, safe_height: float = 0,dwell
         cuts : List[Union[LinearCut,ArcCut]] = list(nx.dfs_preorder_nodes(op.cuts))
         first_cut,*rest = cuts
         yield GCodeRapidMove(Z=safe_height)
-        yield GCodeRapidMove(X=first_cut.start[0],Y=first_cut.start[1],Z=safe_height)
+        yield GCodeRapidMove(X=first_cut.start[0],Y=first_cut.start[1])
         yield from first_cut.gcode(True,True,True,True,True)
         if len(rest) > 0:
             for (pre,now) in windowed(cuts,2):
@@ -28,10 +28,11 @@ def safe_height_solver(job : Job, machine: Machine, safe_height: float = 0,dwell
                 jump = np.linalg.norm(pstop-nstart) > eps
                 if jump:
                     yield GCodeLinearMove(Z=safe_height)
-                    yield GCodeRapidMove(X=now.start[0],Y=now.start[1],Z=safe_height)
+                    yield GCodeRapidMove(X=now.start[0],Y=now.start[1])
                 yield from now.gcode(jump,True,pre.feed != now.feed, pre.speed != now.speed,False)
         yield GCodeRapidMove(Z=safe_height)
-        yield GCodeRapidMove(X=0,Y=0)
-        yield GCodeStopSpindle()
-        yield GCodePauseProgram()
+        if pause_every_op:
+            yield GCodeRapidMove(X=0,Y=0)
+            yield GCodeStopSpindle()
+            yield GCodePauseProgram()
     yield GCodeEndProgram()
